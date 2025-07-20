1877201e6d708b1b43161a13685e8571358e78b8
Harshit Raj <harshitintelligent@gmail.com>
2025-07-20 13:05:55 +0530

    completed all assignments

--- Week 2/Assignment 1/Maven, Spring and Spring Boot/Customer-SpringBatch/src/main/java/com/customer/spring/batch/repository/CustomerRepository.java
+++ Week 2/Assignment 1/Maven, Spring and Spring Boot/Customer-SpringBatch/src/main/java/com/customer/spring/batch/repository/CustomerRepository.java
@@ -1,6 +1,6 @@
-package com.javatechie.spring.batch.repository;
+package com.customer.spring.batch.repository;
 
-import com.javatechie.spring.batch.entity.Customer;
+import com.customer.spring.batch.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
